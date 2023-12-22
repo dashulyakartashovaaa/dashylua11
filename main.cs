ccb7fdc4645f019104deffdff8e587bc95f29d8f
dashulyakartashovaaa <154631550+dashulyakartashovaaa@users.noreply.github.com>
2023-12-22 23:54:54 +0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+  }
+}
+from string import (
+    punctuation, whitespace, digits,
+    ascii_lowercase, ascii_uppercase)
+
+
+def is_valid_password(password):
+    new_password = password.strip()
+
+    MIN_SIZE = 6
+    MAX_SIZE = 20
+    password_size = len(new_password)
+
+    if password_size < MIN_SIZE or password_size > MAX_SIZE:
+        return False
+
+    valid_chars = {'-', '_', '.', '!', '@', '#', '$', '^', '&', '(', ')'}
+    invalid_chars = set(punctuation + whitespace) - valid_chars
+
+    for char in invalid_chars:
+        if char in new_password:
+            return False
+
+    password_has_digit = False
+
+    for char in password:
+        if char in digits:
+            password_has_digit = True
+            break
+
+    if not password_has_digit:
+        return False
+
+    password_has_lowercase = False
+
+    for char in password:
+        if char in ascii_lowercase:
+            password_has_lowercase = True
+            break
+
+    if not password_has_lowercase:
+        return False
+
+
+    password_has_uppercase = False
+
+    for char in password:
+        if char in ascii_uppercase:
+            password_has_uppercase = True
+            break
+
+    if not password_has_uppercase:
+        return False
+
+    return True
